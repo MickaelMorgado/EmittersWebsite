cfbe7bdb4809c5e82bd2dd32882bd41d59c55c52
MickaelMorgado <mickael.morgado31@gmail.com>
2024-05-07 22:40:36 +0100

    revert next related installations

--- node-projects/app.js
+++ node-projects/app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  var message = 'It works yey!\n',
+  var message = 'It works yey 2!\n',
     version = 'NodeJS ' + process.versions.node + '\n',
     response = [message, version].join('\n');
   res.end(response);
