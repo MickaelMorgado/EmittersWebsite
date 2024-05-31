02efb098c06be1b8a8d8a5416856501cfc7ac6f1
MickaelMorgado <mickael.morgado31@gmail.com>
2024-05-31 20:54:14 +0100

    test

--- node-projects/src/Test.tsx
+++ node-projects/src/Test.tsx
@@ -1,37 +1,69 @@
-import axios, {isCancel, AxiosError} from 'axios';
+import React, { useEffect } from 'react';
+import { SciChartSurface, NumericAxis, XyDataSeries, FastCandlestickRenderableSeries, ENumericFormat } from 'scichart';
+// import { appTheme } from './SciChartTheme';  // Assuming you have a theme file
+import axios from 'axios';
 
 const Test = () => {
-
-    const API_KEY = '12345678900987654321-abc34135acde13f13530';
-    const BASE_URL = 'https://api-fxpractice.oanda.com/v3';
-
-    const symbol = 'EUR_USD';
-    const granularity = 'M1';
-
-    const getHistoricalData = async () => {
-        try {
-            const response = await axios.get(`${BASE_URL}/instruments/${symbol}/candles`, {
-                params: {
-                    granularity: granularity,
-                    count: 100
-                },
-                headers: {
-                    'Authorization': `Bearer ${API_KEY}`
-                }
-            });
-            console.log('res')
-            console.log(response.data);
-        } catch (error) {
-            console.log('er')
-            console.error(error);
+    useEffect(() => {
+        const initSciChart = async () => {
+            const { sciChartSurface, wasmContext } = await SciChartSurface.create('scichart-root');
         }
-    };
+    })
 
-    getHistoricalData();
+    const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=EURUSD&interval=1min&apikey=UWFE4L3M53KZ4YEX';
 
-    return (
-        <></>
-    );
+axios.get(url)
+    .then(response => {
+        debugger
+        const timeSeries = response.data['Time Series (1min)'];
+        if (!timeSeries) {
+            console.error('Time Series data is not available');
+            return;
+        }
+        const data = Object.keys(timeSeries).map(time => ({
+            date: new Date(time),
+            open: parseFloat(timeSeries[time]['1. open']),
+            high: parseFloat(timeSeries[time]['2. high']),
+            low: parseFloat(timeSeries[time]['3. low']),
+            close: parseFloat(timeSeries[time]['4. close']),
+        }));
+
+          const xValues = data.map(d => d.date.getTime());
+          const openValues = data.map(d => d.open);
+          const highValues = data.map(d => d.high);
+          const lowValues = data.map(d => d.low);
+          const closeValues = data.map(d => d.close);
+
+          const dataSeries = new XyDataSeries(wasmContext, {
+            xValues,
+            openValues,
+            highValues,
+            lowValues,
+            closeValues,
+          });
+
+          const candlestickSeries = new FastCandlestickRenderableSeries(wasmContext, {
+            dataSeries,
+            strokeThickness: 1,
+            dataPointWidth: 0.7,
+            // upStroke: appTheme.VividGreen,
+            // upFill: appTheme.VividGreen,
+            // downStroke: appTheme.MutedRed,
+            // downFill: appTheme.MutedRed,
+          });
+
+          sciChartSurface.renderableSeries.add(candlestickSeries);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+
+      // Dispose of SciChartSurface when the component is unmounted
+      //return () => initSciChart();
+
+  return (
+    <div id="scichart-root" style={{ width: '100%', height: '350px' }}></div>
+  );
 }
 
-export default Test;
+export default Test
