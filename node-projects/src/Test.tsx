c6eba8d8790a9acfaa3a7be05b0f893a73597305
MickaelMorgado <mickael.morgado31@gmail.com>
2024-06-10 22:26:13 +0100

    working example of scichart

--- node-projects/src/Test.tsx
+++ node-projects/src/Test.tsx
@@ -1,158 +1,78 @@
-import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import {
-  CategoryAxis,
-  FastOhlcRenderableSeries,
+  EllipsePointMarker,
+  FastLineRenderableSeries,
+  MouseWheelZoomModifier,
+  NumberRange,
   NumericAxis,
-  OhlcDataSeries,
   SciChartDefaults,
   SciChartJsNavyTheme,
-  SciChartSurface
+  SciChartSurface,
+  SweepAnimation,
+  XyDataSeries,
+  ZoomExtentsModifier,
+  ZoomPanModifier
 } from 'scichart';
 
-SciChartDefaults.enableResampling = false;
-SciChartDefaults.performanceWarnings = false;
-
 const Test = () => {
-  
-  // const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=EURUSD&interval=1min&apikey=UWFE4L3M53KZ4YEX';
-  const url = 'https://api.binance.com/api/v3/klines?symbol=LTCBTC&interval=1m';
-
-  enum DataConversionEnum {
-    Binance = 'BINANCE',
-    AlphaVantage = 'ALPHAVANTAGE'
-  }
-
-  interface IPriceData {
-    time: number,
-    open: number,
-    high: number,
-    low: number,
-    close: number
-  }
-  
-  let structuredData: IPriceData[] = [];
-
-  const structuringData = (dataResponse: any, dataConversionEnum: DataConversionEnum): IPriceData[] => {
-    switch (dataConversionEnum) {
-      case DataConversionEnum.Binance:
-        return dataResponse.map((item: any) => ({
-          /* https://developers.binance.com/docs/binance-spot-api-docs/rest-api#klinecandlestick-data
-            [
-              1499040000000,      // Kline open time
-              "0.01634790",       // Open price
-              "0.80000000",       // High price
-              "0.01575800",       // Low price
-              "0.01577100",       // Close price
-              "148976.11427815",  // Volume
-              1499644799999,      // Kline Close time
-              "2434.19055334",    // Quote asset volume
-              308,                // Number of trades
-              "1756.87402397",    // Taker buy base asset volume
-              "28.46694368",      // Taker buy quote asset volume
-              "0"                 // Unused field, ignore.
-            ]
-          */
-            time: item[0], // Unix timestamp in milliseconds
-            open: parseFloat(item[1]),
-            high: parseFloat(item[2]),
-            low: parseFloat(item[3]),
-            close: parseFloat(item[4])
-        }));
-      case DataConversionEnum.AlphaVantage:
-        return [];
-      default:
-        return [];
-    }
-  };
-
-  // Dispose of SciChartSurface when the component is unmounted
-  // return () => initSciChart();
+  const sciChartSurfaceRef = useRef<SciChartSurface | null>(null);
 
   useEffect(() => {
     const initSciChart = async () => {
-      SciChartSurface.UseCommunityLicense()
+      // Enable resampling for better performance
+      SciChartDefaults.enableResampling = true;
+
+      // Tell SciChart where to get webassembly files from
+      SciChartSurface.useWasmFromCDN();
+      SciChartSurface.UseCommunityLicense();
+      
+      // Initialize SciChartSurface
       const { sciChartSurface, wasmContext } = await SciChartSurface.create('scichart-root', {
         theme: new SciChartJsNavyTheme(),
+        title: 'SciChart.js First Chart',
+        titleStyle: { fontSize: 22 }
       });
 
-      // Create an X,Y Axis and add to the chart
-      sciChartSurface.xAxes.add(new CategoryAxis(wasmContext));
-      sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { labelPrefix: "$", labelPrecision: 2 }));
+      sciChartSurfaceRef.current = sciChartSurface;
 
-      axios.get(url)
-        .then(response => {
-          structuringData(
-            response.data,
-            DataConversionEnum.Binance,
-          )
+      // Create XAxis and YAxis
+      const growBy = new NumberRange(0.1, 0.1);
+      sciChartSurface.xAxes.add(new NumericAxis(wasmContext, { axisTitle: 'X Axis', growBy }));
+      sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { axisTitle: 'Y Axis', growBy }));
 
-          /*
-            const timeSeries = response.data['1m'];
-            if (!timeSeries) {
-              console.error('Time Series data is not available');
-              return;
-            }
-          */
+      // Create a line series with some initial data
+      const lineSeries = new FastLineRenderableSeries(wasmContext, {
+        stroke: 'steelblue',
+        strokeThickness: 3,
+        dataSeries: new XyDataSeries(wasmContext, {
+          xValues: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+          yValues: [0, 0.0998, 0.1986, 0.2955, 0.3894, 0.4794, 0.5646, 0.6442, 0.7173]
+        }),
+        pointMarker: new EllipsePointMarker(wasmContext, { width: 11, height: 11, fill: '#fff' }),
+        animation: new SweepAnimation({ duration: 300, fadeEffect: true })
+      });
 
-          /*
-            const data = Object.keys(structuredData).map((key: string, value, object) => {
-              date: new Date(item[0]),
-              open: parseFloat(item[1]),
-              high: parseFloat(item[2]),
-              low: parseFloat(item[3]),
-              close: parseFloat(item[4]),
-              // object[key]
-            });
-          */
-          
-          // const xValues = structuredData.map(d => d.time.getTime());
-          const xValues = structuredData.map(d => d.time);
-          const openValues = structuredData.map(d => d.open);
-          const highValues = structuredData.map(d => d.high);
-          const lowValues = structuredData.map(d => d.low);
-          const closeValues = structuredData.map(d => d.close);
+      sciChartSurface.renderableSeries.add(lineSeries);
 
-          // Create a OhlcDataSeries with open, high, low, close values
-          /*
-            const dataSeries = new OhlcDataSeries(wasmContext, {
-              xValues: xValues,
-              openValues: openValues,
-              lowValues: lowValues,          
-              highValues: highValues,
-              closeValues: closeValues,
-            });
-          */
-
-          const dataSeries = new OhlcDataSeries(wasmContext, {
-            xValues: xValues,
-            openValues: openValues,
-            lowValues: lowValues,
-            closeValues: closeValues,
-            highValues: highValues,
-          });
-          
-          // Create and add the OhlcSeries series
-          const ohlcSeries = new FastOhlcRenderableSeries(wasmContext, {
-            dataSeries,
-            strokeThickness: 1,
-            dataPointWidth: 0.7,
-          });
-
-          sciChartSurface.renderableSeries.add(ohlcSeries);
-          
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }
+      // Add interaction modifiers
+      sciChartSurface.chartModifiers.add(
+        new MouseWheelZoomModifier(),
+        new ZoomPanModifier(),
+        new ZoomExtentsModifier()
+      );
+    };
 
     initSciChart();
+
+    // Cleanup
+    return () => {
+      if (sciChartSurfaceRef.current) {
+        sciChartSurfaceRef.current.delete();
+      }
+    };
   }, []);
 
-  return (
-    <div id="scichart-root" style={{ width: '100%', height: '100%' }}></div>
-  );
-}
+  return <div id="scichart-root" style={{ width: '100%', height: '500px' }} />;
+};
 
 export default Test;
