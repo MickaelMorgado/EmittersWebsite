5bc14dc11f184ecdec5ff7bc20c6b36900d335e8
Mickael Morgado <mickael.morgado31@gmail.com>
2025-06-08 21:30:16 +0100

    docker + vite + tailwind setup

--- node-projects/src/main.tsx
+++ node-projects/src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { SciChartSurface, SciChart3DSurface } from "scichart";
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import { ThemeProvider } from './components/theme-provider'
 
-SciChartSurface.loadWasmFromCDN();
-SciChart3DSurface.loadWasmFromCDN();
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-theme">
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
---
+++ node-projects/src/pages/PNLCalendar/index.tsx
@@ -0,0 +1,13 @@
+
+import TradingCalendar from "./full-calendar";
+
+const PNLCalendar = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1>PNL Calendar</h1>
+      <TradingCalendar />
+    </div>
+  );
+};
+
+export default PNLCalendar;
