ec31835f25cd83a7827691d9cb56f783c3aa0087
Mickael Morgado <mickael.morgado31@gmail.com>
2025-06-08 21:30:16 +0100

    docker + vite + tailwind setup

---
+++ node-projects/src/pages/HipsExample/index.tsx
@@ -0,0 +1,14 @@
+import MainWebsitePage from '../../MainWebsitePage';
+
+const HipsExamplePage = () => {
+  return (
+    <div className="project-page">
+      <h1>Hips Example</h1>
+      <div className="project-content">
+        <MainWebsitePage />
+      </div>
+    </div>
+  );
+};
+
+export default HipsExamplePage;
