f64be228308af3a67e25c4128390361f46255286
Mickael Morgado <mickael.morgado31@gmail.com>
2025-10-07 12:42:30 +0100

    fix assets path

--- node-projects/my-app/src/app/mika/Printing3D.tsx
+++ node-projects/my-app/src/app/mika/Printing3D.tsx
@@ -9,7 +9,7 @@ import { Mesh, Object3D } from "three";
 
 function PrintingModel(props: any) {
   const group = useRef<Object3D>(null!);
-  const { scene, animations } = useGLTF("/assets/js/threejs/models/A1Mini.gltf");
+  const { scene, animations } = useGLTF("/tools/js/threejs/models/A1Mini.glb");
   const { actions, mixer } = useAnimations(animations, group);
 
   // Set up glow on specific parts
