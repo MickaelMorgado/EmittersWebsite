10821517dfacdcdc93b1b4ff02588dd4f9b76005
Mickael Morgado <mickael.morgado31@gmail.com>
2025-10-07 20:43:08 +0100

    fix

--- node-projects/my-app/eslint.config.mjs
+++ node-projects/my-app/eslint.config.mjs
@@ -17,7 +17,7 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
       "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-explicit-any": "off",
-      "no-unused-vars": 1,
+      "no-unused-vars": 0,
     },
   },
 ];
