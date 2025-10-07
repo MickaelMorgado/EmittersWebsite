df386deef918226ef9eb0e45a74517676f15b08b
Mickael Morgado <mickael.morgado31@gmail.com>
2025-10-07 11:51:51 +0100

    fix: upgraded back to react19

--- node-projects/my-app/src/app/threejs/page.tsx
+++ node-projects/my-app/src/app/threejs/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 import { Canvas } from '@react-three/fiber';
-import { Leva } from 'leva';
 import dynamic from 'next/dynamic';
-import BlockChainVisualizer from './BlockChainVisualizer';
+//import BlockChainVisualizer from './BlockChainVisualizer';
 const WireframeGrid = dynamic(
   () => import('./WireframeGrid'),
   { ssr: false }
@@ -11,10 +10,9 @@ const WireframeGrid = dynamic(
 const ThreeJSCryptoVisualizerPage = () => {
 return (
     <div className="fixed inset-0 w-full h-full bg-black">
-      <Leva hidden />
       <Canvas camera={{ position: [0, 10, 10], fov: 75 }}>
         <WireframeGrid />
-        <BlockChainVisualizer />
+        {/* <BlockChainVisualizer /> */}
       </Canvas>
       <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, zIndex: 10 }}>
       </div>
@@ -22,4 +20,4 @@ return (
   );
 };
 
-export default ThreeJSCryptoVisualizerPage;
+//export default ThreeJSCryptoVisualizerPage;
